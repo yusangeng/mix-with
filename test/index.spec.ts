3d38f95a622b4818a2bf878101c11ba8c2252f05
yusangeng <yusangeng@outlook.com>
2019-02-15 18:57:03 +0800

    完善单元测试

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,47 +4,56 @@ import { mix, C, $ } from '../src'
 
 chai.should()
 
+class Foo {
+  a: number = 1
+  b () { return this.a }
+}
+
+const Bar = <T>(superclass: C<T>) => {
+  class Bar extends $(superclass) {
+    c: number = 2
+    d () { return this.c }
+  }
+  
+  return Bar as C<T & Bar>
+}
+
+const Baz = <T>(superclass: C<T>) => {
+  class Baz extends $(superclass) {
+    e: number = 3
+    f () { return this.e }
+  } 
+
+  return Baz as C<T & Baz>
+}
+
 describe('mix', () => {
   it('should throw', async () => {
     const fn = () => mix(1 as any)
     fn.should.throws()
   })
 
-  it('result class should have all properties & methods', async () => {
-    class Foo {
-      a: number = 1
-      b () { return this.a }
-    }
-
-    const Bar = <T>(superclass: C<T>) => {
-      class Bar extends $(superclass) {
-        c: number = 2
-        d () { return this.c }
-      }
-      
-      return Bar as C<T & Bar>
-    }
-
-    const Baz = <T>(superclass: C<T>) => {
-      class Baz extends $(superclass) {
-        e: number = 3
-        f () { return this.e }
-      } 
-
-      return Baz as C<T & Baz>
-    }
-
+  it('FooBarBaz result class should have all properties & methods', async () => {
     const FooBarBaz = mix(Foo).with(Bar, Baz)
     const fbz = new FooBarBaz()
 
     fbz.b().should.be.eq(1)
     fbz.d().should.be.eq(2)
     fbz.f().should.be.eq(3)
+  })
 
+  it('BarBaz result class should have all properties & methods', async () => {
     const BarBaz = mix().with(Bar, Baz)
     const bz = new BarBaz()
 
     bz.d().should.be.eq(2)
     bz.f().should.be.eq(3)
+  })
+
+  it('FooCopy result class should have all properties & methods', async () => {
+    const FooCopy = (mix(Foo).with as any)()
+    const f = new FooCopy()
+
+    f.b().should.be.eq(1)
   })
 })
